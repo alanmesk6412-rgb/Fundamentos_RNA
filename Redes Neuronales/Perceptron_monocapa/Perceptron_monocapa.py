ec1d62f5f2bb8f9b3a62371cbc22d07b73c14d7e
alanmesk6412-rgb <alan.mesk6412@gmail.com>
2026-02-07 20:12:44 -0600

    Update Perceptron_monocapa.py
    Se eliminaron los prints para hacer el debugging

--- Redes Neuronales/Perceptron_monocapa/Perceptron_monocapa.py
+++ Redes Neuronales/Perceptron_monocapa/Perceptron_monocapa.py
@@ -1,5 +1,5 @@
 import numpy as np
-import os
+
 def eval(N,W):
   print(np.dot(N,W))
   y = np.heaviside(np.dot(N,W),0)
@@ -21,35 +21,24 @@ Q = salidas.readline()
 Y = np.array([float(num) for num in Q.split(",")])
 
 t = 0.05
-
-print(W)
-N = np.array([1.,1.,1.])
-print(f"EL RESULTADO ANTES DEL APRENDIZAJE ES: {eval(N,W)}")
-print(Y[3])
-
+N = np.array([0.,0.,1.])
 print("======================================================================================")
+print(f"EL RESULTADO ANTES DEL APRENDIZAJE ES: {eval(N,W)}")
+print("======================================================================================\n\n")
 for j in range(3):
   k=0
-  print("PRIMERA ITERACIÓN")
   entradas.seek(0)
   for linea in entradas:
-    print(f"Linea {k}")
     X = np.array([float(num) for num in linea.split(",")])
-
     y = np.heaviside(np.dot(X,W),0)
-    print("COMIENZO DE AJUSTES")
     for i in range(3):
       W[i] = W[i] + t*(Y[k] -y)
-      print(Y[k])
     k = k+1
-    
-    print("FIN DE AJUSTES")
-
-
 
 print("======================================================================================")
-print(W)
 print(F"EL RESULTADO DESPUÉS DEL APRENDIZAJE ES: {eval(N,W)}")
+print("======================================================================================")
+
 
 
 
